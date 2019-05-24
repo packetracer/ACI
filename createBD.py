d6edc8690a3c92e7eadc11027b26e184d85d0425
Ryan <rpere16@gmail.com>
2019-05-24 18:39:09 -0500

    Update createBD.py

--- createBD.py
+++ createBD.py
@@ -1,5 +1,16 @@
-
-hostname = '<APIC IP OR FQDN>'
+#CREATE BRIDGE DOMAINS BASED ON LIST OF VLANS
+import requests
+from jinja2 import Template
+#use pip install jinja2 if you get a module error
+import sys
+import login
+import getpass
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings(urllib3.exceptions.SNIMissingWarning)
+urllib3.disable_warnings(urllib3.exceptions.InsecurePlatformWarning)
+#CONSTANTS
+hostname = 'apic.lgh.org'
 
 #LOGIN AND GET COOKIE
 def getCookie():
@@ -8,16 +19,15 @@ def getCookie():
         cookie = login.login(user,pwd)
         if "ERROR" not in cookie:
                 return cookie
-#VARIABLES
-#THIS CREATES BD for ONE TENANT/VRF COMBINATION PER EXECUTION BASED ON VLAN NAMES LISTED IN vlans.list
+#TAKE INPUT OF TENANT AND VRF FROM USER
 tenant = raw_input("Tenant Name: ")
 vrf = raw_input("VRF Name: ")
+#READ LIST OF VLANS FROM VLANS.LIST
 vlans = []
-f = open("vlans.list","r")
 for vlan in f.read().splitlines():
         vlans.append(vlan)
-
-#CREATE FLOODING BD (CHANGE VALUES TO MEET YOUR NEEDS)
+        
+#CREATE BRIDGE DOMAINS BASED ON LIST OF VLANS
 def createBD(vlan,tenant,vrf,cookie):
         urltemplate = "https://{{hostname}}/api/node/mo/uni/tn-{{tenant}}/BD-{{VLAN}}-BD.json"
         ut = Template(urltemplate)
@@ -57,12 +67,20 @@ def createBD(vlan,tenant,vrf,cookie):
         out.append(result.text)
         return out
 
-
+###########
 cookie = getCookie()
+suc=0
+fail=0
+#FOR EACH VLAN, CREATE A BRIDGE DOMAIN
 for vlan in vlans:
         result = createBD(vlan,tenant,vrf,cookie)
         if result[0]==200:
                 print result[1]+" created successfully"
+                suc+=1
         else:
                 print "Failure creating "+result[1]
                 print result[2]
+                fail+=1
+#PRINT NUMBER OF SUCCESSFUL CREATES AND NUMBER OF FAILS                
+print "Success: "+str(suc)
+print "Fail: " +str(fail)
