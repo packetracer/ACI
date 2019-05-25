e16fb9ac8fd2d3e9fd8a577e05da49d5d4fadf7d
Ryan <rpere16@gmail.com>
2019-05-24 23:04:31 -0500

    Update listEPGs.py

--- listEPGs.py
+++ listEPGs.py
@@ -27,7 +27,6 @@ ap = raw_input("App Profile: ")
 def listEPGs(tenant,ap,cookie):
         cookies = {'APIC-cookie':cookie}
         urlt = """https://apic.lgh.org/api/node/mo/uni/tn-{{TENANT}}/ap-{{AP}}.json?query-target=subtree&target-subtree-class=fvAEPg&query-target-filter=and(not(wcard(polUni.dn, "__ui_")),eq(fvAEPg.isAttrBasedEPg,"false"))&query-target=subtree&target-subtree-class=fvRsProv,fvRsCons,tagAliasInst"""
-
         ut = Template(urlt)
         url = ut.render(TENANT=tenant,AP=ap)
         result = requests.get(url,cookies=cookies,verify=False)
@@ -36,7 +35,7 @@ def listEPGs(tenant,ap,cookie):
         print "TOTAL ENTRIES: " + str(last)
         for i in range(0,last):
                 print "EPG#{}: ".format(str(i+1))+ (result['imdata'][i]['fvAEPg']['attributes']['name'])
-#ATTEMPT TO LOGIN, AFTER MAX_ATTEMPTS, KILL SCRIPT
+
 def attemptLogin():
         cookie = getCookies()
         i = 1
@@ -44,11 +43,12 @@ def attemptLogin():
                 if i == MAX_ATTEMPT:
                         print "Too many attempts.  Goodbye."
                         sys.exit()
-                elif cookie == None:
+                                        elif cookie == None:
                         i+=1
                         cookie = getCookies()
                 else:
+                        return cookie
                         break
 
-attemptLogin()
+cookie = attemptLogin()
 listEPGs(tenant,ap,cookie)
